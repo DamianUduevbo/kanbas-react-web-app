e25ba4d3c9ebeb8bc6d4ef40444109e46fdccb19
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2023-10-19 03:30:35 -0400

    JavaScript and React.js Assignment 3

---
+++ src/Kanbas/Database/index.js
@@ -0,0 +1,17 @@
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import users from "./users.json";
+import enrollments from "./enrollments.json";
+import grades from "./grades.json";
+
+// eslint-disable-next-line
+export default {
+  courses,
+  modules,
+  assignments,
+  users,
+  enrollments,
+  grades
+};
+
